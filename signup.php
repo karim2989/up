1a188a528a069364d02295ba52b8a5a46cefb867
karim ben fraj <karim2989@gmail.com>
2023-03-04 23:59:33 +0100

    init

---
+++ signup.php
@@ -0,0 +1,24 @@
+<?php
+    $name1 = $_POST["name1"];
+    $name2 = $_POST["name2"];
+    $email = $_POST["email"];
+    $pwd = $_POST["pwd"];
+
+    $tel = $_POST["tel"];
+    $adr = $_POST["adr"];
+    $cin = $_POST["cin"];
+    $bd = $_POST["bd"];
+
+    $conn = new mysqli('localhost', 'root', 'root', 'cabinet');
+    if ($conn->connect_error) {
+        die("server error: " . $conn->connect_error);
+    }
+    if ($conn->query("Insert into personne (email,pwd,nom,prenom,naissance,address,tel,cin) values ('".$email."','".hash("md5",$pwd)."','".$name1."','".$name2."','".$bd."','".$adr."','".$tel."','".$cin."')")) {
+        print("success");
+    }
+    else{
+        die("error");
+    }
+    
+
+?>
